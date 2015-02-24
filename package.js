f489ef3af5c60e4a3a0a9a07a02bd8cf0ee25f42
Pusher Robot <jack@semantic-ui.com>
2015-02-24 14:34:33 -0500

    Updated component to version 1.10.2

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-site',
   summary : 'Semantic UI - Site: Single component release',
-  version : '1.10.1',
+  version : '1.10.2',
   git     : 'git://github.com/Semantic-Org/UI-Site.git',
 });
 
