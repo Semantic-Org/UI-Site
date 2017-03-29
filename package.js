8aefa3c3cef7e24e8ac327dbc97062102cc68d23
Jack Lukic <jack@semantic-ui.com>
2017-03-29 06:43:00 -0400

    Updated component to version 2.2.10

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-site',
   summary : 'Semantic UI - Site: Single component release',
-  version : '2.2.8',
+  version : '2.2.10',
   git     : 'git://github.com/Semantic-Org/UI-Site.git',
 });
 
